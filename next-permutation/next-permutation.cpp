98cdc8e810e150deab30b0cdbf6d107b19898200
shivanshgupta999 <70778790+shivanshgupta999@users.noreply.github.com>
2021-11-10 21:35:01 +0530

    Time: 4 ms (75.98%), Space: 11.9 MB (93.59%) - LeetHub

--- next-permutation/next-permutation.cpp
+++ next-permutation/next-permutation.cpp
@@ -6,34 +6,25 @@ public:
        
         int n=arr.size();
       
-        
         int x;
         for( x=n-2;x>=0;x--){
             if(arr[x]<arr[x+1]){
-                
                 break;
             }
         }
-        if(x<0){
-            
-       
-            int i=0,j=n-1;
-             while(i<j){
-                 swap(arr[i],arr[j]);
-                 i++;
-                 j--;
-             }
-            
-            return  ;
-        } 
-        
+        if(x==-1){
+            sort(arr.begin(),arr.end());
+            return;
+        }
         for(int i=n-1;i>x;i--){
             if(arr[i]>arr[x]){
                 swap(arr[i],arr[x]);
                 break;
             }
         }
+        
        reverse(arr.begin()+x+1,arr.end());
+      
          return;
     }
 };
