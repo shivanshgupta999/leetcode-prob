5b7cc650023eca2322e3b7a9be4a22f689114bb2
shivanshgupta999 <70778790+shivanshgupta999@users.noreply.github.com>
2021-11-17 12:05:34 +0530

    Time: 16 ms (56.38%), Space: 11.9 MB (14.16%) - LeetHub

--- two-sum/two-sum.cpp
+++ two-sum/two-sum.cpp
@@ -1,30 +1,20 @@
 class Solution {
 public:
-   vector < int> twoSum(vector< int>& nums, int target) {
- int low=0,high=nums.size()-1,n1,n2;
- vector< int> v,nums2;
- nums2=nums;
- sort(nums2.begin(),nums2.end());
- while (low<high){
-	 if (nums2[low]+nums2[high]<target) {
-			 low++;
-		 }
-	 else if(nums2[low]+nums2[high]>target){
-			 high--;
-		 }
-	 else{   //nums2[low]+nums2[high]==target
-			 n1=nums2[low]; 
-			 n2=nums2[high];
-			 break;
- 		} 
- 	}
-	
-for (int i=0;i< nums.size() ; i++){
-         if (nums[i]==n1)
-         v.push_back(i);
-         else if (nums[i]==n2)
-         v.push_back(i);
-		 }
-		 return v;
+   vector < int> twoSum(vector< int>& nums, int t) {
+unordered_map<int,int> mp;
+       int n=nums.size();
+        vector < int> ans(2);
+       
+       for(int i=0;i<n;i++){
+           mp[nums[i]]=i;
+       }
+       for(int i=0;i<n;i++){
+           if(mp.count(t-nums[i])&& i!=mp[t-nums[i]]){
+               ans[0]=i;
+               ans[1]=mp[t-nums[i]];
+               break;
+           }
+       }
+       return ans;
 		 }
 };	
