6c196651a4a878761c33781a6252057424924933
shivanshgupta999 <70778790+shivanshgupta999@users.noreply.github.com>
2022-03-11 02:10:46 +0530

    Time: 886 ms (6.25%), Space: 6.6 MB (90.79%) - LeetHub

--- 686-repeated-string-match/686-repeated-string-match.cpp
+++ 686-repeated-string-match/686-repeated-string-match.cpp
@@ -1,37 +1,51 @@
 class Solution {
 public:
-    bool check(string a, string& b){
-        int j=0,n1=a.size(),n2=b.size();
-        for(int i=0;i<=n1-n2;i++){
-           for (j = 0; j < b.size(); j++){
-                if(a[i+j]!=b[j]){
-                    break;
-                }       
-           }
-               if(j==n2)
+/*    bool check(string a, string &b){
+        int j=0;
+        for(int i=0;i<a.size();i++){
+           
+                if(a[i]==b[j]){
+                    j++;
+                }
+             else{
+                 if(j>0)
+                     i-=j;
+                 j=0;
+             }
+        if(j==b.size()){
             return true;
         }
+         }
         return false;
-    }
-    int repeatedStringMatch(string a, string b) {
-        float n1=a.size();
-        int n2=b.size();
-      
-        int cnt= ceil(n2/n1);
-       
-        string t=a;
-        for(int i=1;i<cnt;i++){
-            t+=a;
+    }*/
+    int repeatedStringMatch(string A, string B) {
+        int m = A.length();
+    int n = B.length();
+ 
+    int count;
+    bool found = false;
+ 
+    for (int i = 0; i < m; i++) {
+        int j = i;
+        int k = 0;
+        count = 1;
+ 
+        while (k < n && A[j] == B[k]) {
+            if (k == n - 1) {
+                found = true;
+                break;
+            }
+            j = (j + 1) % m;
+ 
+            if (j == 0)
+                count++;
+ 
+            k++;
         }
-     
-       
-           if(check(t,b))
-               return cnt;
-           
-         if(check(t+a,b))
-           return cnt+1;
-       
-         return -1;
+        if (found)
+            return count;
+    }
+    return -1;
         
     }
         
