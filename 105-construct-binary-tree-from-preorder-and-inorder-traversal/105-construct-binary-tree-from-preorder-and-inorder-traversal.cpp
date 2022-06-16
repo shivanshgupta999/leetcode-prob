48f231bfb6fe91a41837f18bd082d872ae5390ef
shivanshgupta999 <70778790+shivanshgupta999@users.noreply.github.com>
2022-06-16 14:24:24 +0530

    Time: 45 ms (35.96%), Space: 26.4 MB (46.70%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -11,25 +11,21 @@
  */
 class Solution {
 public:
-     TreeNode* b(vector<int>& preorder, vector<int>& inorder,int pres,int pree,int ins,int ine){
+     TreeNode* b(vector<int>& preorder, vector<int>& inorder,int pres,int pree,int ins,int ine,unordered_map<int,int>& mp){
        if(pres>pree|| ins>ine){
            return NULL;
        }
        TreeNode* root=new TreeNode(preorder[pres]);
-         int cnt=0;
-         int t=ins;
-        while(cnt<=ine-ins && preorder[pres]!=inorder[t]  ){
-            t++;
-            cnt++;
-        }
+         int cnt=mp[preorder[pres]]-ins;
+         
          // cnt=2
          // pre r,9,1,20,15,7,8   pres=0,1 pree=7,        
          //                                                                           pres,ins    pree,ine
                                                             //[1,      2,                4,      3]
 //                                                            [1,      2,                3  ,    4]
          //  in 1,9,r,8,15,20,7   ins=0 ine=7               ans - 1,null,
-       root->left= b(preorder,inorder,pres+1,(pres+cnt),ins,ins+cnt-1);
-       root->right= b(preorder,inorder,pres+cnt+1,pree,ins+cnt+1,ine);
+       root->left= b(preorder,inorder,pres+1,(pres+cnt),ins,ins+cnt-1,mp);
+       root->right= b(preorder,inorder,pres+cnt+1,pree,ins+cnt+1,ine,mp);
          
         return root;
     }
@@ -38,7 +34,10 @@ public:
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
        
         int n=preorder.size();
-        
-return b(preorder,inorder,0,n-1,0,n-1);
+        unordered_map<int,int> mp;
+        for(int i=0;i<n;i++){
+            mp[inorder[i]]=i;
+        }
+return b(preorder,inorder,0,n-1,0,n-1,mp);
     }
 };
